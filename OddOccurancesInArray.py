227705fda057b8e9f2363fa038350a12f32ce81a
Aidan Cruickshank <aidangcruickshank@gmail.com>
2023-10-22 00:09:38 -0400

    Add OddOccurancesInArray solution.

---
+++ OddOccurancesInArray.py
@@ -0,0 +1,9 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+#Note: Too easy hahaha, thanks Esteban lmao
+def solution(A):
+    xor = 0
+    for i in A: 
+        xor = xor ^ i
+    return xor
