fb15daa466823d54465f3f8bd9c3dcf611a76830
Aidan Cruickshank <aidangcruickshank@gmail.com>
2023-10-21 23:58:52 -0400

    Update CyclicRotation for 100% score solution.

--- CyclicRotation.py
+++ CyclicRotation.py
@@ -2,16 +2,14 @@
 # print("this is a debug message")
 
 # Note: For this question I initally lost points for not 
-# considering the empty array case. 
+# considering the empty array case.
 
 def solution(A, K):
-
-    newA = [None] * len(A)
-    K = K % len(A)
-    
     if len(A) == 0:
         return A
     else:
+        newA = [None] * len(A)
+        K = K % len(A)
         for i in range(len(A)):
             if i + K <= len(A) - 1:
                 newA[i + K] = A[i]
