6f27ec46df9a08863f895953eaf3e7309be2cfac
Aidan Cruickshank <aidangcruickshank@gmail.com>
2023-10-21 23:54:42 -0400

    Add CyclicRotation solution.

---
+++ CyclicRotation.py
@@ -0,0 +1,23 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+# Note: For this question I initally lost points for not 
+# considering the empty array case. 
+
+def solution(A, K):
+
+    newA = [None] * len(A)
+    K = K % len(A)
+    
+    if len(A) == 0:
+        return A
+    else:
+        for i in range(len(A)):
+            if i + K <= len(A) - 1:
+                newA[i + K] = A[i]
+            else:
+                newK = K - (len(A) - i)
+                newA[newK] = A[i]
+        return newA
+
+
